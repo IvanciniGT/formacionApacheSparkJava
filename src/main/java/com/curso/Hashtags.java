eff3003033d32df3753ea45b019496e28351b845
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2024-07-24 11:43:16 +0200

    refactorizado

---
+++ src/main/java/com/curso/Hashtags.java
@@ -0,0 +1,13 @@
+package com.curso;
+
+import java.util.List;
+
+public class Hashtags {
+
+    private static final List<String> PALABRAS_PROHIBIDAS = List.of("caca", "culo","pedo","pis", "mierda");
+
+    public static void main(String[] args) {
+
+    }
+
+}
